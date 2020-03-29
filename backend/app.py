513344bd38e4c86b957b5646bf7d54668e3fc396
Anooj Shah <thisisanooj@gmail.com>
2020-03-29 00:21:54 -0500

    Add tags of messages

--- backend/app.py
+++ backend/app.py
@@ -6,8 +6,12 @@ import pymongo
 from flask import Flask, request, abort, jsonify
 from pymongo import MongoClient
 import requests
+import random
+import json
 from datetime import datetime
 from zoom import zoom_create_meeting, zoom_list_meetings
+
+from bson.json_util import dumps
 
 
 client = pymongo.MongoClient('mongodb+srv://anooj101:rowdyhacks@cluster0-v8p0t.gcp.mongodb.net/test?retryWrites=true&w=majority')
@@ -20,23 +24,43 @@ users = db['users']
 
 app = Flask(__name__)
 
-@app.route('/join_meeting', methods=['GET'])
-def join_meeting():
-    meeting_name='PyMongo is fun, you guys'
-    #lookup meetings_documents for zoom_id
-    #zoom api to join meeting
-    meeting = meetings.find_one({'meeting_name': meeting_name})
-    if(meeting):
-        zoom_id=meeting['zoom_id']
-        print(meeting, zoom_id)
-        #zoom join using zoom_id
-        return zoom_id
-    return "<h1>Bad meeting</h1>"
+@app.route('/list_tags', methods=['GET'])
+def list_tags():
+    # meetings_arr = (list(meetings.find()))
+    tags={}
+    for i in meetings.find():
+        for j in i['tags']:
+            if j in tags:
+                tags[j].append(i)
+            else:
+                tags[j] = [i]
+    print(tags)
+    return dumps(meetings_arr)
 
-@app.route('/create_meeting', methods=['GET'])
+@app.route('/list_meetings', methods=['GET'])
+
+
+def list_meetings():
+    meetings_arr = dumps(list(meetings.find()))
+
+    return meetings_arr
+
+# @app.route('/join_meeting', methods=['GET'])
+# def join_meeting():
+#     meeting_name='PyMongo is fun, you guys'
+#     #lookup meetings_documents for zoom_id
+#     #zoom api to join meeting
+#     meeting = meetings.find_one({'meeting_name': meeting_name})
+#     if(meeting):
+#         zoom_id=meeting['zoom_id']
+#         print(meeting, zoom_id)
+#         #zoom join using zoom_id
+#         return zoom_id
+#     return "<h1>Bad meeting</h1>"
+
+@app.route('/create_meeting', methods=['POST'])
 def create_meeting():
     # headers = {'meeting_name':'value', 'host_name':'sdf', 'tags' :'', 'description' : "sadf"}
-    print("h1")
     response = request.get_json()
     # response = request.headers
     # get geolocation
@@ -45,7 +69,6 @@ def create_meeting():
     tags = response['tags'] # list of tags
     description = response['description']
     time = datetime.now()
-    print("h2")
     meeting = {
         'zoom_id': zoom_create_meeting(meeting_name, time),
         'meeting_name': meeting_name,
@@ -53,15 +76,11 @@ def create_meeting():
         'time': time,
         'host_name': host_name,
         'description': description,
+        'num_people' : random.randint(205,500),
         'geolocation_lat':0,
         'geolocation_long':0
     }
-    print("h3")
     result = meetings.insert_one(meeting)
     print('One meeting scheduled: {0}'.format(result.inserted_id))
     return "<h1>Hello</h1>"
-
-# @app.route('/createcm')
-# def createcm():
-#    summary  = request.args.get('summary', None)
-#    change  = request.args.get('change', None)
+# list_tags()
--- backend/old_zoom.py
+++ backend/old_zoom.py
@@ -1,10 +1,16 @@
 import requests
 
-r = requests.get('http://127.0.0.1:5000/create_meeting', json = {'meeting_name':'value', 'host_name':'sdf', 'tags' :'', 'description' : "sadf"})
+r = requests.get('http://127.0.0.1:5000/create_meeting', json = {'meeting_name':'test8', 'host_name':'sdf', 'tags' :['business','hobbies','creativity'], 'description' : "sadf"})
+r = requests.get('http://127.0.0.1:5000/create_meeting', json = {'meeting_name':'test9', 'host_name':'sdf', 'tags' :['DIY','creativity','c'], 'description' : "sadf"})
+r = requests.get('http://127.0.0.1:5000/create_meeting', json = {'meeting_name':'test10', 'host_name':'sdf', 'tags' :['gujrati','business','hobbies'], 'description' : "sadf"})
+r = requests.get('http://127.0.0.1:5000/create_meeting', json = {'meeting_name':'test11', 'host_name':'sdf', 'tags' :['creativity','business'], 'description' : "sadf"})
+r = requests.get('http://127.0.0.1:5000/create_meeting', json = {'meeting_name':'test12', 'host_name':'sdf', 'tags' :['DIY','food','gujrati'], 'description' : "sadf"})
 
-print(r)
+#
+# print(r)
+# print(r.content)
+r = requests.get('http://127.0.0.1:5000/list_meetings')
 print(r.content)
-
 # # create meeting: host_id="ID", topic="TOPIC", type="TYPE"
 # # list: user_id
 # # print(user_id)
@@ -30,3 +36,14 @@ print(r.content)
 #
 #     {'uuid': 'am5G181ESlGJ9j4DD6gxPw==', 'id': 219314825, 'host_id': 'W4Y0rZOHRpGl7RhxiBG-VA', 'topic': 'test', 'password': '', 'h323_password': '', 'status': 0, 'option_jbh': False, 'option_start_type': 'video', 'option_host_video': True, 'option_participants_video': True, 'option_cn_meeting': False, 'option_enforce_login': False, 'option_enforce_login_domains': '', 'option_in_meeting': False, 'option_audio': 'both', 'option_alternative_hosts': '', 'option_use_pmi': False, 'type': 2, 'start_time': '2020-03-28T20:29:02Z', 'duration': 60, 'timezone': 'America/Chicago', 'start_url': 'https://zoom.us/s/219314825?zak=eyJ6bV9za20iOiJ6bV9vMm0iLCJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJjbGllbnQiLCJ1aWQiOiJXNFkwclpPSFJwR2w3Umh4aUJHLVZBIiwiaXNzIjoid2ViIiwic3R5IjoxLCJ3Y2QiOiJhdzEiLCJjbHQiOjAsInN0ayI6IlBwcWNfeGktMl8zM1pnRzMxa1o5Y1NsbVdCbTl0cktEUHdZZ21wX2QxcG8uRWdJQUFBRnhJLWdVakFBQUhDQWdaMnc0UlVwaWJWb3hiakJCWVdwWlNHOU9kRTFMVm5ac1FrNXFjbFpHYjBFQURETkRRa0YxYjJsWlV6TnpQUU5oZHpFIiwiZXhwIjoxNTg1NDUyNTQ0LCJpYXQiOjE1ODU0NDUzNDQsImFpZCI6Ikl2OU9mTFJQUjN1NklnYk5zNlVzaHciLCJjaWQiOiIifQ.EaTL7s3mph45eC18N80Zuvt4rzrgwPqoMyTvql3jAos', 'join_url': 'https://zoom.us/j/219314825', 'created_at': '2020-03-29T01:29:02Z'}
 # ]}
+
+
+#
+#
+# [{'_id': ObjectId('5e7fb3ad2220746aeaa93236'), 'zoom_id': 1, 'meeting_name': 'PyMongo is fun, you guys', 'tags': ['Fun', 'Reading'], 'time': '10:34', 'host_name': 'John', 'description': 'description', 'geolocation_lat': 0, 'geolocation_long': 0},
+# {'_id': ObjectId('5e7fbbf87560678d95822225'), 'zoom_id': 2, 'meeting_name': 'meeting 2', 'tags': ['Fun', 'Reading'], 'time': '10:34', 'host_name': 'John', 'description': 'description', 'geolocation_lat': 0, 'geolocation_long': 0},
+# {'_id': ObjectId('5e7fbc1d8f2055e43f7dc30b'), 'zoom_id': 2, 'meeting_name': 'meeting 2', 'tags': ['Fun', 'Reading'], 'time': '10:34', 'host_name': 'John', 'description': 'description', 'geolocation_lat': 0, 'geolocation_long': 0},
+# {'_id': ObjectId('5e801161812a2ff4082bc843'), 'zoom_id': 157191797, 'meeting_name': 'basdf', 'tags': 'sdfdsf', 'time': datetime.datetime(2020, 3, 28, 22, 9, 20, 149000), 'host_name': 'sdf', 'description': 'sadf', 'geolocation_lat': 0, 'geolocation_long': 0},
+# {'_id': ObjectId('5e8012f72133afdbca52dbd3'), 'zoom_id': 409119287, 'meeting_name': 'value', 'tags': '', 'time': datetime.datetime(2020, 3, 28, 22, 16, 6, 682000), 'host_name': 'sdf', 'description': 'sadf', 'geolocation_lat': 0, 'geolocation_long': 0},
+# {'_id': ObjectId('5e8015e5d4229c9dc06bf34a'), 'zoom_id': 168867074, 'meeting_name': 'value', 'tags': '', 'time': datetime.datetime(2020, 3, 28, 22, 28, 36, 735000), 'host_name': 'sdf', 'description': 'sadf', 'geolocation_lat': 0, 'geolocation_long': 0},
+# {'_id': ObjectId('5e8016079f5d01b3f8d2131d'), 'zoom_id': 190138393, 'meeting_name': 'value', 'tags': '', 'time': datetime.datetime(2020, 3, 28, 22, 29, 10, 454000), 'host_name': 'sdf', 'description': 'sadf', 'geolocation_lat': 0, 'geolocation_long': 0}]
